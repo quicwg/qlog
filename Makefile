2a0956c37753d2656086bf4fff232b58edbaba50
Luca Niccolini <luca.niccolini@gmail.com>
2023-01-02 16:51:59 -0800

    Script to validate CDDL for all documents
    the main_schema file is validated on its own,
    both the http and quic event documents also include the main schema
    
    Sample run
    ```
    ~/github/qlog (main*) » make cddl                                                                                  lnicco@Lucas-MacBook-Air
    Validating draft-ietf-quic-qlog-h3-events.md
      OK
    Validating draft-ietf-quic-qlog-main-schema.md
      OK
    Validating draft-ietf-quic-qlog-quic-events.md
      ERROR
      debug with: ./cddl_validate.sh draft-ietf-quic-qlog-quic-events.md
    ```
    
    The quic events file error is fixed in #267

--- Makefile
+++ Makefile
@@ -9,3 +9,23 @@ else
 	git clone -q --depth 10 $(CLONE_ARGS) \
 	    -b main https://github.com/martinthomson/i-d-template $(LIBDIR)
 endif
+
+# run cddl_validate.sh for all drafts_source files 
+cddl: $(drafts_source)
+	@for f in $^; do \
+	    echo "Validating $$f"; \
+	    ./cddl_validate.sh $$f > /tmp/foo 2>&1 ; \
+	    if [ $$? -eq 0 ]; then \
+	        echo "  OK"; \
+	    else \
+	        echo "  ERROR"; \
+					echo "  debug with: ./cddl_validate.sh $$f"; \
+	    fi; \
+	done
+
+# override lib/main.mk clean target, to cleanup json and cddl files
+.PHONY: clean
+clean::
+		$(MAKE) -f $(LIBDIR)/main.mk $@
+		-rm -f \
+	    $(addsuffix -[0-9][0-9].{json$(COMMA)cddl},$(drafts))
---
+++ cddl_validate.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#
+# requires `gem install cddl`
+#
+set -e
+set -o nounset
+
+INPUT_FILE="$1"
+BASE_FILENAME="${INPUT_FILE%.*}"
+CDDL_FILE="${BASE_FILENAME}.cddl"
+CDDL_JSON_FILE="${BASE_FILENAME}.json"
+
+:> $CDDL_FILE
+
+# Extracts CDDL from a markdown file
+# $1: the input (.md) file
+# $2: the output (.cddl) file
+function extract_cddl() {
+    cat $1 | awk 'BEGIN{flag=0} /~~~ cddl/{flag=1; printf "\n"; next} /~~~/{flag=0; next} flag' >> $2
+    #sed -n '/^```cddl/,/^```/p' $1 | sed '1d;$d' >> $2
+}
+
+# Prepend an object with all the unused types to a CDDL file
+# $1: the input (.cddl) file
+function generate_aux_object() {
+    # Generate the list of Unused types
+    unused_types=$(cddl $1 generate 2>&1 | grep Unused | cut -d " " -f 4)
+    # Create an object with all the unused types
+    tmpfile=$(mktemp)
+    echo "AuxObjectWithAllTypesForValidationOnly = {" >> $tmpfile
+    for type in ${unused_types}; do
+      lowercase_type=$(echo ${type} | tr '[:upper:]' '[:lower:]')
+      echo "    ${lowercase_type}_: ${type}" >> $tmpfile
+    done
+    echo -e "}\n" >> $tmpfile
+
+    tmpfile2=$(mktemp)
+    cat $tmpfile $1 > $tmpfile2
+    mv $tmpfile2 $1
+}
+
+if [ $INPUT_FILE != "draft-ietf-quic-qlog-main-schema.md" ]; then
+    # Extracts CDDL from the main schema file
+    extract_cddl draft-ietf-quic-qlog-main-schema.md $CDDL_FILE
+fi
+
+extract_cddl $INPUT_FILE $CDDL_FILE
+generate_aux_object $CDDL_FILE
+# The cddl command doesn't know how to work with .regexp with a give size.
+# We use that with hexstring sometimes, so clean that up
+sed -i '' "s/hexstring .size .*/hexstring/" $CDDL_FILE
+cddl ${CDDL_FILE} json-generate > ${CDDL_JSON_FILE}
